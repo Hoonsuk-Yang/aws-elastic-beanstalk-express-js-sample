6748d5ab0248151cd13c462467b531837a83ad3d
Hoonsuk Yang <hsyang22@hsyang22-macbookair.local>
2022-09-09 16:34:18 +0900

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello tutorial. It is my first step to be a real devops'));
+app.get('/', (req, res) => res.send('Hello tutorial. It is my first step to be a real devops\nWhere do we go now.. '));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
